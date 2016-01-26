e736efdd82a334599d6f25654aa846bd20228c74
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-25 19:15:33 -0800

    first query, inserting data into user table successfully

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var db = require('./config/dbOperations.js');
+var User = require('./controllers/userController.js');
 
 
 var app = express();
@@ -14,35 +15,23 @@ app.use(express.static(__dirname + '/../client'));
 
 // require('./config/routes.js')(app, express);
 
-// var port = process.env.PORT || 8000;
-// app.listen(port);
+var port = process.env.PORT || 8000;
+app.listen(port);
 
 //postgres set up
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/foodbot';
-var client = new pg.Client(connectionString);
 
+var client = new pg.Client(connectionString);
 client.connect();
+
+//create db tables
 var createUserTable = client.query(db.createUserTable);
-createUserTable.on('end', function() { client.end(); });
-
-var client = new pg.Client(connectionString);
-client.connect();
 var createProfileTable = client.query(db.createProfileTable);
-createProfileTable.on('end', function() { client.end(); });
-
-var client = new pg.Client(connectionString);
-client.connect();
 var createRecipeTable = client.query(db.createRecipeTable);
-createRecipeTable.on('end', function() { client.end(); });
-// query.on("error", function (error) {
-//     console.log('error in query', error);
-// });
-// query.on("row", function (row, result) {
-//     console.log('adding row in query')
-//     result.addRow(row);
-// });
 
+
+var addUserToTable = client.query(User.signUpUser);
 
 console.log('Listening...');
 
