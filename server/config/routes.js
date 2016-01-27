af83a8dea9c6d2fefdf525e3ba4f1a6f1ad5e574
giselakayy <giselakottmeier@gmail.com>
2016-01-26 22:02:52 -0800

    Updated Routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,9 +7,8 @@ var helpers = require('./helpers.js');
 
 
 module.exports = function(app, express) {
-
-	app.get('/foodBot/profile/:id', userController.retrieveOneUser); // Add AuthChecker
-  app.get('/foodBot/profile', userController.retrieveAllUsers); // Add AuthChecker
+	
+    // app.get('/foodBot/profile', userController.retrieveAllUsers); // Add AuthChecker
 	app.post('/foodBot/auth/signup', userController.signup);
 	app.post('/foodBot/auth/signin', userController.signin); // Add AuthChecker
 	// app.get('/foodBot/auth/signedin', userController.signedin);
@@ -18,7 +17,7 @@ module.exports = function(app, express) {
 	app.get('/foodBot/recipes/:id',  recipeController.retrieveSuggestedRecipes); // Add AuthChecker
 
 	app.get('/foodBot/profile/:id', profileController.retrieveUserMeals); // Add AuthChecker
-	// app.post('/foodBot/dash/:id', dashboardController.addMeal); // Add AuthChecker
+	app.post('/foodBot/meals/:id', profileController.addUserMeal); // Add AuthChecker
 	// app.get('/foodBot/users/home/:username', dashboardController.getUserProfile) // Add AuthChecker
 
 };
