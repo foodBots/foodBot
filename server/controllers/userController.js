2bd5cea1831f4f432df28a0ab2e3a4207d18ce0e
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-27 19:10:48 -0800

    user passwords encrypted and user session defined upon account creation in database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,33 @@
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/foodbot';
+var auth = require('../config/authOperations.js');
 
 module.exports = {
   signup: function(req, res) {
     var client = new pg.Client(connectionString);
     client.connect();
-    var query = client.query("INSERT INTO Users (password, email) VALUES (crypt('"+req.body.password+"', gen_salt('bf', 8)),'"+req.body.email+"');");
-    // currently not rejecting signup attempts for existing users, instead creates new items if email already exists
-    query.on('end', function(results) {
-      res.send(201);
+    //check if username already exists
+    var checkUserQuery = client.query("SELECT * FROM Users where email ='"+req.body.email+"';", function(err, data) {
+      if (err) {
+        res.status(500).json("We're sorry, an error has occurred");
+      } else if (data.rows.length > 0) {
+        //res.status(400).json('User with that email already exists');
+        res.redirect('/foodBot/auth/signup');
+        // res.send({
+        //   // status: 400,
+        //   // json: 'User with that email already exists',
+        //   redirect: '/signup'
+        // });
+      } else {
+      var createUserQuery = client.query("INSERT INTO Users (password, email) VALUES (crypt('"+req.body.password+"', gen_salt('bf', 8)),'"+req.body.email+"');");    
+      createUserQuery.on('end', function(results) {
+        auth.createSession(req, res, req.body.email)
+        res.status(201).json('User session created');
+        client.end();
+      });
+      }
+    });
+    checkUserQuery.on('end', function(results) {
       client.end();
     });
   }, 
@@ -56,5 +75,6 @@ module.exports = {
     });
   }
 }
+
 
 //)
