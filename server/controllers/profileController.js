56fa08454ac4af321190ed83e270ca97ad012bee
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-28 14:56:30 -0800

    Profile controller serving req body

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -3,82 +3,41 @@ var connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/
 
 module.exports = {
 
-	retrieveUserMeals : function (req, res){
-		// Get User ID
-		var uid = req.params.id;
-
-		// Create Postgress Connection
-		var client = new pg.Client(connectionString);
-		client.connect();
-
-		// Create Query for all recipes user has created or eaten
-		var userRecipesQuery = client.query("SELECT * FROM UserRecipes WHERE profileid = " + uid + "") ;
-
-		// Instantiate User Recipes
-		var userRecipes = [];
-
-		// Push all db userRecipes to userRecipes Array
-		userRecipesQuery.on("row", function (row) {
-			userRecipes.push(row);
-		});
-
-		// After recieved all User Recipes
-		userRecipesQuery.on("end", function () {
-
-			// Instantiate Created and Eaten Array
-			var created = [];
-			var eaten = [];
-
-			// Sort All Recipes By Eaten & Created
-			userRecipes.forEach(function (recipe){
-
-				if (recipe.created){
-					created.push(recipe)
-				}
-				else {
-					eaten.push(recipe)
-				}
-			})
-
-			// Send userRecipes to Client
-			var sendData = {created: created, eaten: eaten }
-			res.send(sendData);
-		});
-
-	},
-
-	addUserMeal : function (req, res){
-
-		// Get Client Data
-		var uid = req.params.id;
-		var recipeAdded = req.body.mealID;
-
-		// Create Postgress Connection
-		var client = new pg.Client(connectionString);
-		client.connect();
-
-		// Create Insert Meal Query
-		var addUserRecipeQuery = client.query("INSERT INTO userRecipes (profileid, recipeid, created) VALUES (" + uid + "," + recipeAdded + ", false)") ;
-		//TODO: MAKE RESTRAINT TO NOT ALLOW DUPLICATES
-
-		// After Added Send Client 200 Status Code
-		addUserRecipeQuery.on("end", function (){
-			res.sendStatus(200);
-		})
-		res.sendStatus(409)
-
-	},
   retrieveOneUser: function(req, res, next) {
     var client = new pg.Client(connectionString);
     client.connect();
     var userId = req.body.id;
-    var query = client.query('SELECT NAME, RESTRICTIONS, ALLERGIES, BUDGET, MATCH FROM Users INNER JOIN Users ON'+userId+'= Profiles.ID;')
+    var query = client.query('SELECT NAME, RESTRICTIONS, ALLERGIES, BUDGET, MATCH FROM Profiles INNER JOIN Users ON'+userId+'= Profiles.ID;')
     query.on('row', function(userData) {
       res.status(200).json(userData);
     });
     query.on('end', function() {
       client.end();
     });
+  },
+  addUserProfile: function() {
+  	//on sign up
+
+  },
+  updateUserProfile: function(req, res, next) {
+  	var prep = req.body.prep;
+  	var budget = req.body.budget;
+  	var type = req.body.type;
+  	var userId = req.body.id;
+  	var client = new pg.Client(connectionString);
+  	client.connect();
+  	var query = client.query("INSERT INTO Profiles (id, cookingTime, foodie, budget) VALUES ('"+userId+"','"+prep+"','"+type+"','"+budget"')");
+  	query.on('end', function() {
+  		client.end();
+  	});
+  	next(
+  	// {
+  	// 	id: userId,
+  	// 	cookingTime: prep,
+  	// 	foodie: type,
+  	// 	budget: budget
+  	// }
+  	)
   },
   retrieveAllUsers: function(req, res, next) {
     console.log('logged in user!', req.session.user);
