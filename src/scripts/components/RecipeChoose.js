8c7c7e0df4363b92ca23f4061afff0e68b907679
Byron Rau <byronrau@gmail.com>
2016-01-29 15:44:03 -0800

    styling card

--- src/scripts/components/RecipeChoose.js
+++ src/scripts/components/RecipeChoose.js
@@ -45,7 +45,7 @@ class Recipe extends React.Component {
               </button>
               <h4 className="modal-title">Modal title</h4>
             </div>
-            <div className="modal-body">                      
+            <div className="modal-body">
               <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
             </div>
             <div className="modal-footer">
@@ -144,7 +144,7 @@ class Recipe extends React.Component {
         <CardActions>
           <RaisedButton label="No" primary={true} onClick={this.next.bind(this, element)} />
           <RaisedButton label="Yes" secondary={true} onClick={this.yes.bind(this, element)} /><br/><br/>
-          <RaisedButton  label="Save and Match!" primary={true} onClick={this.saveMatch.bind(this)} />
+          <RaisedButton  label="Pair and Cook!" primary={true} onClick={this.saveMatch.bind(this)} />
         </CardActions>
         </Card>
 
