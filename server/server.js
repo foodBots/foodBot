f89577de2732a59559b4f917f936afb72787b00e
Gisela <giselakottmeier@gmail.com>
2016-02-01 16:55:19 -0800

    Updated Server.js to match devserver.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+
+var path = require('path');
 var express = require('express');
 var session = require('express-session');
-var path = require('path');
+
+//Extras
 var bodyParser = require('body-parser');
 var db = require('./config/dbOperations.js');
 var User = require('./controllers/userController.js');
 
+//Webpack
+var webpack = require('webpack');
+var config = require('./webpack.config.dev');
+var compiler = webpack(config);
+
 var app = express();
 
+//Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(session({
@@ -16,17 +25,36 @@ app.use(session({
   cookie: { secure: true }
 }))
 
-// app.use(cookieParser());
-app.use(express.static(__dirname + '/../build'));
+//Hot Reloading
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use(express.static(__dirname + '/../client'));
 
 require('./config/routes.js')(app, express);
 
-var port = process.env.PORT || 8000;
-app.listen(port);
+var port = process.env.PORT || 3000;
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
 
 //postgres set up
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgresql://localhost/foodbot';
+var connectionString = process.env.DATABASE_URL || 'postgresql://root:foodrobot@localhost/foodbot';
 
 var client = new pg.Client(connectionString);
 client.connect();
@@ -38,6 +66,5 @@ var createRecipesTable = client.query(db.createRecipesTable);
 var createProfilesTable = client.query(db.createProfilesTable);
 var createUserRecipesTable = client.query(db.createUserRecipesTable);
 var createMatchesQueueTable = client.query(db.createMatchesQueueTable);
-
 
 module.exports = app;
