0e297a2984ee65a0cf7ded24902ed8d7df38c505
giselakayy <giselakottmeier@gmail.com>
2016-02-05 17:28:16 -0800

    Removed IAMDATA & created the search terms controller

---
+++ server/controllers/searchTermsController.js
@@ -0,0 +1,31 @@
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/foodbot';
+var client = new pg.Client(connectionString);
+var foodItems = ['fish', 'salad', 'chicken', 'vegetables']
+module.exports = {
+
+	formatSearchTerms: function () {
+		var foods = "";
+
+		var searchTerms = foodItems.reduce(function (memo, foodName){
+			memo += "('" + foodName + "'), ";
+			return memo;
+		}, foods)
+
+		return searchTerms.substr(0, searchTerms.length-2);
+		// for each fooditem 
+			// add parans
+			// add it to a sting
+	},
+
+	seedSearchTerms: function () {
+	client.connect();  
+		console.log("inserting in RecipeSearchTerms: ", module.exports.formatSearchTerms())
+
+		client.query("INSERT INTO RecipeSearchTerms (name) VALUES " + module.exports.formatSearchTerms() + "", function (err, result){
+			console.log("ERROR:", err)
+		});
+	}
+
+
+}
